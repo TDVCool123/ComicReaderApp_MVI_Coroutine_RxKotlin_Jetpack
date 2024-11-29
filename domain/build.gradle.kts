a5cd1223c0ecc3b0d06c14472ed5ad67090aa78e
TDVCool123 <luisandypp@gmail.com>
2024-11-29 01:15:17 -0800

    fix final

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -7,6 +7,5 @@ dependencies {
   implementation(deps.reactiveX.java)
   implementation(deps.kotlin.coroutinesCore)
   implementation(deps.arrow.core)
-  //implementation("com.chrynan.uri:uri-core-iossimulatorarm64:0.4.0")
   api(deps.uri.core)
 }
